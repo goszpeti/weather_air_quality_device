68c9f30edd2d56221976e079e57322feca442f98
peter-gosztolya <peter.gosztolya@gmail.com>
2021-10-02 16:37:39 +0200

    Remove duplicated files

---
+++ src/waqd/base/component.py
@@ -0,0 +1,135 @@
+#
+# Copyright (c) 2019-2021 Péter Gosztolya & Contributors.
+#
+# This file is part of WAQD
+# (see https://github.com/goszpeti/WeatherAirQualityDevice).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+
+import threading
+import time
+import types
+# this allows to use forward declarations to avoid circular imports
+from typing import TYPE_CHECKING, Callable, Optional
+
+from waqd.base.logger import Logger
+from waqd.base.system import RuntimeSystem
+from waqd.settings import Settings
+
+if TYPE_CHECKING:
+    from waqd.base.components import ComponentRegistry
+
+
+class Component:
+    """ Base class for all components """
+
+    def __init__(self, components: "ComponentRegistry" = None, settings: Settings = None):
+        self._comps = components
+        self._settings = settings  # TODO remove
+        self._logger = Logger()
+        self._runtime_system = RuntimeSystem()
+        self._reload_forbidden = False  # must be set manually in the child class
+        self._disabled = False
+        self._ready = True
+
+    @property
+    def is_ready(self) -> bool:
+        """ Returns true, if component is ready to be used."""
+        return self._ready
+
+    @property
+    def reload_forbidden(self):
+        """
+        When this flag is set, the component signals, that it should not be reloaded,
+        when settings change. This should only be used by components, which use
+        read-only settings and need a long time to initialize.
+        """
+        return self._reload_forbidden
+
+    @property
+    def is_disabled(self):
+        """
+        The component can signal it is disabled, if it does not work correctly
+        and its values are not to be used. (Component will always return an instance)
+        """
+        return self._disabled
+
+    def stop(self):
+        pass
+
+
+class CyclicComponent(Component):
+    """
+    Implements the cyclic updatefor a Component with a separate thread.
+    State can be checked by is_alive.
+    """
+    UPDATE_TIME: int = 0  # in seconds
+    INIT_WAIT_TIME: int = 0  # in seconds
+    STOP_TIMEOUT: int = 2 * UPDATE_TIME
+    MAX_ERROR = 5  # max error before reset
+
+    def __init__(self, components=None, settings=None):
+        super().__init__(components, settings)
+        self._ticker_event = threading.Event()
+        self._update_thread: Optional[threading.Thread] = None
+        self._ready = False
+        self._error_num = 0
+
+    @property
+    def is_alive(self) -> bool:
+        """ Update thread is running, module is OK. """
+        if not self._update_thread:
+            return False
+        if self._update_thread.is_alive() and not self._ticker_event.is_set():
+            return True
+        return False
+
+    def stop(self):
+        """ Stop this module, by sending a stop request. """
+        if self._update_thread:
+            self._ticker_event.set()
+            if self._update_thread.is_alive():
+                self._update_thread.join(self.STOP_TIMEOUT)
+
+    def _start_update_loop(self, init_func: Callable = None,
+                           update_func: Callable = None):
+        """
+        Generic set up function for cyclic thread.
+        Has to be called with own init and update function in child class.
+        """
+        self._update_thread = threading.Thread(name=self.__class__.__name__,
+                                               target=self._update_loop,
+                                               args=[init_func, update_func, ],
+                                               daemon=True)
+        self._update_thread.start()
+
+    def _update_loop(self, init_func: types.FunctionType, update_func: types.FunctionType):
+        """
+        Executes an initializer function, optionally waits
+        and then calls the update function periodically.
+        """
+        time.sleep(self.INIT_WAIT_TIME)
+        if init_func:
+            init_func()
+        self._ready = True
+        while not self._ticker_event.wait(self.UPDATE_TIME):
+            if self._ticker_event.is_set():
+                self._ticker_event.clear()
+                return
+            if self._error_num == self.MAX_ERROR:
+                self._is_disabled = True
+                return
+            update_func()
