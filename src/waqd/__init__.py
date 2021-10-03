36ef1147f7e18ccac0559dca36895ad633201366
peter-gosztolya <peter.gosztolya@gmail.com>
2021-10-03 15:33:03 +0200

    Bump up version to 1.6b1

--- src/waqd/__init__.py
+++ src/waqd/__init__.py
@@ -17,4 +17,4 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
-__version__ = "1.6.0a10"
+__version__ = "1.6.0b1"
