66f7227f63f00058d0c6172c421dabb18b371f8b
peter-gosztolya <peter.gosztolya@gmail.com>
2021-10-10 21:38:56 +0200

    User DockerClient in tests

--- test/integration/test_updater.py
+++ test/integration/test_updater.py
@@ -2,7 +2,7 @@ import os
 import tarfile
 import platform
 from distutils.file_util import copy_file
-import docker
+from docker.client import DockerClient
 import pytest
 from waqd import config
 from waqd import __version__ as VERSION
@@ -15,7 +15,7 @@ WAQD_IMAGE = "goszpeti/waqd_installed:1"
 @pytest.mark.updater
 def testInstallInDockerWithoutGUI(base_fixture):
     """ Start an installation with the installer running without the updater ui. """
-    client = docker.from_env()
+    client = DockerClient.from_env()
 
     docker_base_cmd = f"docker build {str(base_fixture.base_path)} -t {WAQD_IMAGE} -f ./test/testdata/auto_updater/dockerfile_install"
     if platform.system() == "Linux":
