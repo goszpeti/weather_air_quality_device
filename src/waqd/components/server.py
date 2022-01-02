fa68ac1f7a3656a1d090b1086b604080a64c5c93
peter-gosztolya <peter.gosztolya@gmail.com>
2022-01-02 11:01:52 +0100

    * Use paste as server * Add main server site for displaying sensor values * support stopping the server

--- src/waqd/components/server.py
+++ src/waqd/components/server.py
@@ -1,9 +1,11 @@
-from bottle import request, run, route
+from bottle import request, run, route, get, post, default_app
 
 
 from waqd.base.component import Component
 from typing import TYPE_CHECKING
 from threading import Thread
+
+from waqd.components.sensors import BarometricSensor, CO2Sensor, HumiditySensor, TempSensor
 if TYPE_CHECKING:
     from waqd.base.component_reg import ComponentRegistry
     from waqd.settings import Settings
@@ -12,10 +14,44 @@ class Server(Component):
 
     def __init__(self, components: "ComponentRegistry" = None, settings: "Settings" = None):
         super().__init__(components=components, settings=settings)
+        self._app = default_app()
         self._run_thread = Thread(
             name="RunServer", target=self._run_server, daemon=True)
         self._run_thread.start()
-        self._reload_forbidden = True  # must be set manually in the child class
+
+    def get_remote_ext_values():
+        pass
+
+    def _entrypoint(self):
+        response =  """
+        <!DOCTYPE html>
+        <html>
+        <head>
+        <style>
+        body { background-color:#7CB9E8}
+        h1   { color: white;}
+        p    {color: white;}
+        </style>
+        </head>
+        <body>
+        <h1>Welcome to the Weather Air Quality Device!</h1>
+        <p>&nbsp;</p>
+        """
+        for sensor_name in self._comps.get_names():
+            sensor = self._comps.get(sensor_name)
+            sensor_class = sensor.__class__
+            name = str(sensor_class).replace("'>", "").split(".")
+
+            if issubclass(sensor_class, TempSensor):
+                response += f"<p>{name[-1]} temperature: {sensor.get_temperature()}</p>"
+            if issubclass(sensor_class, HumiditySensor):
+                response += f"<p>{name[-1]} humidity: {sensor.get_humidity()}</p>"
+            if issubclass(sensor_class, CO2Sensor):
+                response += f"<p>{name[-1]} CO2: {sensor.get_co2()}</p>"
+            if issubclass(sensor_class, BarometricSensor):
+                response += f"<p>{name[-1]} pressure: {sensor.get_pressure()}</p>"
+        response += "</body></html>"
+        return response
 
 
     def _receive_sensor_values(self):
@@ -33,16 +69,25 @@ class Server(Component):
         except:
             self._logger.debug(f"Server: Invalid response for /remoteSensor: {str(data)}")
 
-        if request.fullpath == "remoteExtSensor":
+        if "remoteExtSensor" in request.fullpath:
             comp_ctrl.components.remote_exterior_sensor.read_callback(temp, hum)
-        elif request.fullpath == "remoteIntSensor":
+        elif "remoteIntSensor" in  request.fullpath:
             comp_ctrl.components.remote_interior_sensor.read_callback(temp, hum)
 
 
     def _run_server(self):
         route('/remoteExtSensor', 'POST', self._receive_sensor_values)
         route('/remoteIntSensor', 'POST', self._receive_sensor_values)
-        run(host='localhost', port=8080, debug=True)
+        # route('/remoteExtSensor', 'GET', self._receive_sensor_values)
+        # route('/remoteIntSensor', 'GET', self._receive_sensor_values)
+        route('/', 'GET', self._entrypoint)
+        # Can't start server rom bottle, because it does not support stopping it without a hack
+        from paste import httpserver
+        self._server = httpserver.serve(self._app, host='0.0.0.0', port='8080', daemon_threads=True, start_loop=False)
+        self._server.serve_forever()
+
+    def stop(self):
+        self._server.server_close()
 
 
 
