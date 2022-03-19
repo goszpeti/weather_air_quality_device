25a717b131efcdcd4c99b07b9011e406bdf0790c
peter-gosztolya <peter.gosztolya@gmail.com>
2022-03-19 18:03:53 +0100

    Bump up version to 1.6.0b4

--- test/components/test_sensor.py
+++ test/components/test_sensor.py
@@ -7,9 +7,11 @@ from waqd.settings.settings import Settings
 def testServer(base_fixture):
     settings = Settings(base_fixture.testdata_path / "integration")
     comps = ComponentRegistry(settings)
-    #Server(comps, settings)
-    url = 'http://localhost:8080/remoteExtSensor'
-    myobj = {"api_ver": "0.1", 'temp': "25.1", "hum": "80.2"}
+    # Server(comps, settings)
+    # while True:
+    #     pass
+    url = 'http://192.168.1.104:8080/remoteExtSensor'
+    myobj = {"api_ver": "0.1", 'temp': "25.1", "hum": "10.2"}
 
     x = requests.post(url, json=myobj)
     print(x)
