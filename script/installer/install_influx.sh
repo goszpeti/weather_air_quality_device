09204681b5269c9b70f13d1334fcda1f87f281ef
Peter Gosztolya <peter.gosztolya@gmail.com>
2023-02-25 20:27:12 +0100

    Update influxdb installer

--- script/installer/install_influx.sh
+++ script/installer/install_influx.sh
@@ -6,10 +6,13 @@ if  [ $retVal -eq 0 ];then
     exit 0
 fi
 
-curl https://repos.influxdata.com/influxdb.key | gpg --dearmor | sudo tee /usr/share/keyrings/influxdb-archive-keyring.gpg >/dev/null
-sudo tee /etc/apt/sources.list.d/influxdb.list <<< "deb [signed-by=/usr/share/keyrings/influxdb-archive-keyring.gpg] https://repos.influxdata.com/debian $(lsb_release -cs) stable"
-sudo apt-get update # needed, so sources are updaed with the new key!
-sudo apt-get install influxdb2 -y
+# influxdata-archive_compat.key GPG fingerprint:
+#     9D53 9D90 D332 8DC7 D6C8 D3B9 D8FF 8E1F 7DF8 B07E
+wget -q https://repos.influxdata.com/influxdata-archive_compat.key
+echo '393e8779c89ac8d958f81f942f9ad7fb82a25e133faddaf92e15b16e6ac9ce4c influxdata-archive_compat.key' | sha256sum -c && cat influxdata-archive_compat.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg > /dev/null
+echo 'deb [signed-by=/etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg] https://repos.influxdata.com/debian stable main' | sudo tee /etc/apt/sources.list.d/influxdata.list
+
+sudo apt-get update && sudo apt-get install influxdb2
 sudo systemctl unmask influxdb
 sudo systemctl enable influxdb
 sudo systemctl start influxdb
