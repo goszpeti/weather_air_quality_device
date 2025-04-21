eeb4978a3b5c1807cdfb5a4e9d49ca5c7f006337
goszpeti <peter.gosztolya@gmail.com>
2025-04-21 19:39:21 +0200

    Add weather ui routes update

--- src/waqd/ui/web2/weather_main/routes.py
+++ src/waqd/ui/web2/weather_main/routes.py
@@ -1,19 +1,44 @@
-from fastapi import Request, APIRouter
+from pathlib import Path
+
+from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 
-from ..templates import render_spa
-from ..api.sensor.v1.connector import SensorRetrieval
-from ..templates import simple_template
+from ..templates import render_spa, sub_template
 
 rt = APIRouter()
 
+current_path = Path(__file__).parent.resolve()
+
+
 @rt.get("/weather", response_class=HTMLResponse)
 async def root(request: Request):
-    content = interior_card()
-    content = simple_template("weather_main/waqd.html", {"interior_card": content})
+    interior = sub_template("interior.html", {}, current_path, True)
+    exterior = sub_template("exterior.html", {}, current_path, True)
+    forecast = sub_template("forecast.html", {}, current_path, True)
+    content = sub_template(
+        "waqd.html",
+        {
+            "cards": [
+                {
+                    "name": "Interior",
+                    "background": "static/gui_bgrs/background_interior2.jpg",
+                    "content": interior,
+                    "endpoint": "/api/sensor/interior?units=True",
+                },
+                {
+                    "name": "Exterior",
+                    "background": "static/weather_bgrs/cloudy_night_bg.jpg",
+                    "content": exterior,
+                    "endpoint": "/api/sensor/exterior?units=True",
+                },
+                {
+                    "name": "Forecast",
+                    "background": "static/gui_bgrs/background_s7.jpg",
+                    "content": forecast,
+                    "endpoint": "/api/sensor/exterior?units=True",
+                },
+            ]
+        },
+        current_path,
+    )
     return render_spa(request, content)
-
-
-def interior_card():
-    values = SensorRetrieval()._get_interior_sensor_values(units=True)
-    return simple_template("weather_main/components/interior_card.html", {})
