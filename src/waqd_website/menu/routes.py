d6e9217e65893ad7eacf8d22111cf94762d2b501
goszpeti <peter.gosztolya@gmail.com>
2025-06-14 10:35:34 +0200

    Minor fixes for website deployment

--- src/waqd_website/menu/routes.py
+++ src/waqd_website/menu/routes.py
@@ -1,23 +1,17 @@
 from pathlib import Path
-from typing import Annotated
 
 from fastapi import APIRouter
 from fastapi.responses import HTMLResponse
 
-from waqd.web.authentication import (
-    User,
-    user_plain_check
-)
 from waqd.web.templates import base_template
-from waqd.web.authentication import PermissionChecker
 
 rt = APIRouter()
 
 current_path = Path(__file__).parent.resolve()
 
 @rt.get("/", response_class=HTMLResponse)
-async def menu(user: Annotated[User, user_plain_check]):
-    menu_content = ""
+async def menu():
+    user = ""
     menu_content = base_template(
         "views/menu.html",
         {
